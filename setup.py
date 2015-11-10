af951ff4d6155ce530ec15f3cfce7e053918f16c
James Lucas <LucasJ94@hotmail.co.uk>
2015-11-09 22:14:24 -0800

    Adding setup and tidying.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='ImageEncrypter',
+      version='0.1',
+      description='Image Encryption',
+      author='James Lucas',
+      packages=['ImageEncryption'],
+     )
